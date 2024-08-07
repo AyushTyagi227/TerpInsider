e1ae10e7f80522eca8506b3c38106d7ad2e687ea
aymaanhussain <aymaanh415@gmail.com>
2024-08-07 19:04:56 -0400

    started working on the design of the website. Added couple of buttons plus search box and title

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          TerpInsider
-        </p>
-        <p>These are some initial changes before we start coding TerpInsider</p>
+      <div className="line"></div>
+        <button className="home-button">Home</button>
+        <p className="title">TerpInsider</p>
+        <button className="club-buttons">Clubs!</button>
+        <button className="classes-buttons">Classes!</button>
+        <button className="other-buttons">Other!</button>
+        <input className="search-box" placeholder="Search for UMD Clubs, Fraternities, and more!"></input>
       </header>
     </div>
   );
